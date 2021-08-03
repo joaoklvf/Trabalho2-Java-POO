c0b1ca6caa0ded6ab62b6d2c7c5f58864c9122aa
Joao <joaoklvf@gmail.com>
2021-08-02 23:19:48 -0300

    joao

---
+++ Trab2/src/trab2/Trab2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trab2;
+
+/**
+ *
+ * @author Joao
+ */
+public class Trab2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
